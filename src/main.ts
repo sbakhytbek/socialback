261d4b65df4a538648cb39fc16d35c850dc36d55
Sanzhar <95479740+sbakhytbek@users.noreply.github.com>
2026-01-12 14:51:14 +0500

    12.01.2026_1449

--- src/main.ts
+++ src/main.ts
@@ -18,15 +18,14 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
   app.enableCors({ 
-    origin: ['http://localhost:4200','http://192.168.10.237:4200','http://192.168.10.49:4200', 'http://192.168.10.62:3000', 'http://192.168.10.134:4200'],
+    origin: ['http://localhost:4200','http://192.168.10.237:4200','http://192.168.10.49:4200', 'http://192.168.10.62:3000', 'http://192.168.10.134:4200', 'http://192.168.10.148:4200'],
     credentials: true,
   })
-
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   app.use('/media', express.static(join(__dirname, '..', 'media')));
 
   // await app.listen(8888, '192.168.10.49');
-  await app.listen(8888, '192.168.10.134');
+  await app.listen(8888, '192.168.10.148');
 }
 bootstrap();
